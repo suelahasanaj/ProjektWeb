e4f333f26cd310214c8b7e0a9945a1d2225b7110
Erta Zogu <erta.zogu8@gmail.com>
2024-01-14 14:13:44 +0100

    header.html file at patient folder and patient-header.css file

--- patient/index.php
+++ patient/index.php
@@ -1,28 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/animations.css">  
-    <link rel="stylesheet" href="../css/main.css">  
-    <link rel="stylesheet" href="../css/admin.css">
-        
-    <title>Kreu</title>
-    <style>
-        .dashbord-tables{
-            animation: transitionIn-Y-over 0.5s;
-        }
-        .filter-container{
-            animation: transitionIn-Y-bottom  0.5s;
-        }
-        .sub-table,.anime{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-    </style>
-    
-    
-</head>
+<?php
+include("header.html");
+?>
+
 <body>
     <?php
     include("session-start.php");
